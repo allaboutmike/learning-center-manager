b6fc3486b84df23423fbbd7cbdf5d65b9a6cd00d
Yodit-Weldegeorgise <60828026+Yodit-Weldegeorgise@users.noreply.github.com>
2026-02-05 16:52:39 -0500

    created api folder with spring starter

---
+++ api/learning-center-main/src/main/java/com/learningcenter/learningcentermain/LearningCenterMainApplication.java
@@ -0,0 +1,13 @@
+package com.learningcenter.learningcentermain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCenterMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningCenterMainApplication.class, args);
+	}
+
+}
---
+++ api/learning-center-main/src/test/java/com/learningcenter/learningcentermain/LearningCenterMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningcenter.learningcentermain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCenterMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
