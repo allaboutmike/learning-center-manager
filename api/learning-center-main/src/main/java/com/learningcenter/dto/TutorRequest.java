00fdcedbcf9ead624dba677d1eb2630225a0b59c
Kareem42 <kareem4209@gmail.com>
2026-02-08 19:53:30 -0600

    created layered architecture and API documentation.

---
+++ api/learning-center-main/src/main/java/com/learningcenter/dto/TutorRequest.java
@@ -0,0 +1,10 @@
+package com.learningcenter.dto;
+
+public class TutorRequest {
+    /*
+    TutorRequest Object
+    Handles all request for tutor searches via id, availability, and/or grade level.
+
+
+    */
+}
