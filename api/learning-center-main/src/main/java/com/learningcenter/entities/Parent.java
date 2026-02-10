c8286018e667f4d61a42c58f148f89ef31c065a8
ermonio <moni.osborne@yahoo.com>
2026-02-10 17:00:04 -0600

    TUT-28 Add specific imports.

--- api/learning-center-main/src/main/java/com/learningcenter/entities/Parent.java
+++ api/learning-center-main/src/main/java/com/learningcenter/entities/Parent.java
@@ -1,6 +1,14 @@
 package com.learningcenter.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+
 
 import java.util.ArrayList;
 import java.util.List;
