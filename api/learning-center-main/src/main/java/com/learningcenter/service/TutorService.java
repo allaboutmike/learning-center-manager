3c7026af1a259503c4f8758723a60fec6658b82b
Yodit-Weldegeorgise <60828026+Yodit-Weldegeorgise@users.noreply.github.com>
2026-02-14 00:20:46 -0600

    Add tutor availablity endpoint

--- api/learning-center-main/src/main/java/com/learningcenter/service/TutorService.java
+++ api/learning-center-main/src/main/java/com/learningcenter/service/TutorService.java
@@ -1,8 +1,10 @@
 package com.learningcenter.service;
 
 import com.learningcenter.dto.TutorResponse;
+import com.learningcenter.dto.TutorTimeSlotResponse;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,4 +34,10 @@ public class TutorService {
 
     }
 
+    public List<TutorTimeSlotResponse> getTutorAvailability(Long tutorId) {
+        List<TutorTimeSlotResponse> timeSlots = new ArrayList<>();
+        timeSlots.add(new TutorTimeSlotResponse(1L, tutorId, 1L, LocalDateTime.of(2026, 2, 14, 17, 0), LocalDateTime.of(2026, 2, 14, 18, 0)));
+        timeSlots.add(new TutorTimeSlotResponse(2L, tutorId, 2L, LocalDateTime.of(2026, 2, 16, 18, 0), LocalDateTime.of(2026, 2, 16, 19, 0)));
+        return timeSlots;
+    }
 }
