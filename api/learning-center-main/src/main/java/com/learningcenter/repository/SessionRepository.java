a836632ff62aaea0d03713215b7793553912d188
Brandon Hamilton <bham2015@gmail.com>
2026-02-13 22:29:40 -0600

    Added parameters to CrudRepository

--- api/learning-center-main/src/main/java/com/learningcenter/repository/SessionRepository.java
+++ api/learning-center-main/src/main/java/com/learningcenter/repository/SessionRepository.java
@@ -1,6 +1,7 @@
 package com.learningcenter.repository;
 
+import com.learningcenter.entities.Session;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SessionRepository extends CrudRepository {
+public interface SessionRepository extends CrudRepository<Session, Long> {
 }
