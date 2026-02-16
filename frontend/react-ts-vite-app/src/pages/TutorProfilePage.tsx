a9fbbec5d528ca3a862b2be249068565d0f1764f
ermonio <moni.osborne@yahoo.com>
2026-02-16 07:58:09 -0600

    TUT-19 Add Mock Data and connect it to tutor profile page and tutor search screen and write out logic for profile page.

--- frontend/react-ts-vite-app/src/pages/TutorProfilePage.tsx
+++ frontend/react-ts-vite-app/src/pages/TutorProfilePage.tsx
@@ -1,31 +1,53 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useLearningCenterAPI } from "../hooks/useLearningCenterAPI";
-import type { Tutor } from "../types/tutor";
+import { tutor, type Tutor } from "../types/tutor";
+import { useState } from "react";
 
 export default function TutorProfilePage() {
-    const { tutorId } = useParams();
-    if (!tutorId) {
-        return <p> Invalid tutor id</p>;
-    }
-    const tutor = useLearningCenterAPI<Tutor>(`/api/tutors/${tutorId}`);
+  const { tutorId } = useParams();
 
+  if (!tutor.tutorId) {
+      return <p> Invalid tutor id. Please go back and search for tutors here: <Link to="/"> Search all Tutors </Link></p>;
+  }
+  const tutorRoute = useLearningCenterAPI<Tutor>(`/api/tutors/${tutor.tutorId}`);
 
+  if (!tutor) return <p>Loading...</p>;
 
-    if (!tutor) return <p>Loading...</p>;
+  const [selectedTimeSlot, setSelectedTimeSlot] = useState(-1);
 
-    return (
-        <div style={{ textAlign: "center", marginTop: 50 }}>
-            <h1>{tutor.name}</h1>
-            <img
-                src={tutor.profilePictureUrl}
-                alt={tutor.name}
-                style={{ width: 120, borderRadius: "50%" }}
-            />
-            <p>Rating: {tutor.avgRating}</p>
-            <p>Reviews: {tutor.reviewCount}</p>
-            <p>
-                Grades: {tutor.minGradeLevel} - {tutor.maxGradeLevel}
-            </p>
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", marginTop: 50 }}>
+      <img
+        src={tutor.profilePictureUrl}
+        alt={tutor.name}
+        style={{ width: 120, borderRadius: "50%" }}
+      />
+      <h1>{tutor.name}</h1>
+      <p>
+        Grades taught: {tutor.minGradeLevel} - {tutor.maxGradeLevel}
+      </p>
+      <p>{tutor.tutorSummary}</p>
+      <p>Rating: {tutor.avgRating}</p>
+      <p>ReviewCount: {tutor.reviewCount}</p>
+      
+      <h3>Reviews:</h3>
+      {tutor.reviewCount !== 0 ? tutor.reviewCount : "No reviews available"}
+      <ul>
+        <h2>Availability:</h2>
+        {tutor.AvailableTimeSlots.map((timeslot, index) => (
+          <li
+            className={
+              selectedTimeSlot === index && timeslot.tutorTimeslotId !== 0
+                ? "underline"
+                : ""
+            }
+            key={timeslot.tutorTimeslotId}
+            onClick={() => setSelectedTimeSlot(index)}
+          >
+            {timeslot.timeslotId}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
