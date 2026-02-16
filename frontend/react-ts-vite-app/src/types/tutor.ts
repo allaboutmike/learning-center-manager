58962e6347eab125a6811ef6404d4d70c7831bd5
ermonio <moni.osborne@yahoo.com>
2026-02-16 05:42:25 -0600

    TUT-19 Add fields to tutor type and create Tutortimeslot type

--- frontend/react-ts-vite-app/src/types/tutor.ts
+++ frontend/react-ts-vite-app/src/types/tutor.ts
@@ -1,9 +1,16 @@
+import type { TutorTimeslot } from "./tutorTimeslot";
+
+
+
 export type Tutor = {
   tutorId: number;
   name: string;
   profilePictureUrl: string;
+  tutorSummary: string;
   avgRating: number;
   reviewCount: number;
+  reviews: string[];
   minGradeLevel: number;
   maxGradeLevel: number;
+  AvailableTimeSlots: TutorTimeslot[];
 };
