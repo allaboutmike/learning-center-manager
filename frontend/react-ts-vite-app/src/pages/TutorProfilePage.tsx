e6dd4769ce9f4a7b5992ecbce6fe4b6ca7986d3f
ermonio <moni.osborne@yahoo.com>
2026-02-17 22:49:39 -0600

    TUT-19 add select timeslot useState, add availability hook, and logic for tutor count displayed

--- frontend/react-ts-vite-app/src/pages/TutorProfilePage.tsx
+++ frontend/react-ts-vite-app/src/pages/TutorProfilePage.tsx
@@ -1,31 +1,55 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useLearningCenterAPI } from "../hooks/useLearningCenterAPI";
-import type { Tutor } from "../types/tutor";
+import { type Tutor } from "../types/tutor";
+import { type TutorTimeslot } from "../types/tutor";
+import { useState } from "react";
 
 export default function TutorProfilePage() {
-    const { tutorId } = useParams();
-    if (!tutorId) {
-        return <p> Invalid tutor id</p>;
-    }
-    const tutor = useLearningCenterAPI<Tutor>(`/api/tutors/${tutorId}`);
+  const { tutorId } = useParams();
 
+  if (!tutorId) {
+      return <p> Invalid tutor id. Please go back and search for tutors here: <Link to="/"> Search all Tutors </Link></p>;
+  }
+  const tutor = useLearningCenterAPI<Tutor>(`/api/tutors/${tutorId}`);
+  const availability = useLearningCenterAPI<TutorTimeslot[]>(`/api/tutors/${tutorId}/availability`);
 
+  if (!tutor) return <p>Loading...</p>;
 
-    if (!tutor) return <p>Loading...</p>;
+  const [selectedTimeSlot, setSelectedTimeSlot] = useState(-1);
 
-    return (
-        <div style={{ textAlign: "center", marginTop: 50 }}>
-            <h1>{tutor.name}</h1>
-            <img
-                src={tutor.profilePictureUrl}
-                alt={tutor.name}
-                style={{ width: 120, borderRadius: "50%" }}
-            />
-            <p>Rating: {tutor.avgRating}</p>
-            <p>Reviews: {tutor.reviewCount}</p>
-            <p>
-                Grades: {tutor.minGradeLevel} - {tutor.maxGradeLevel}
-            </p>
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", marginTop: 50 }}>
+      <img
+        src={tutor.profilePictureUrl}
+        alt={tutor.name}
+        style={{ width: 120, borderRadius: "50%" }}
+      />
+      <h1>{tutor.name}</h1>
+      <p>
+        Grades I teach: {tutor.minGradeLevel} - {tutor.maxGradeLevel}
+      </p>
+      <p>{tutor.tutorSummary}</p>
+      <p>Rating:{tutor.avgRating}</p>
+      
+      <h3>Reviews: </h3>
+      <p> {tutor.reviewCount}</p>
+      {tutor.reviewCount !== 0 ? tutor.reviewCount : "No reviews available"}
+      <ul>
+        <h2>Availability:</h2>
+        {availability && availability.map((timeslot, index) => (
+          <li
+            className={
+              selectedTimeSlot === index
+                ? "bg-blue-500 text-white"
+                : ""
+            }
+            key={timeslot.tutorTimeslotId}
+            onClick={() => setSelectedTimeSlot(index)}
+          >
+            {timeslot.timeslotId}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- frontend/react-ts-vite-app/src/types/tutor.ts
+++ frontend/react-ts-vite-app/src/types/tutor.ts
@@ -1,9 +1,17 @@
+import type { TutorTimeslot } from "./tutorTimeslot.ts";
+import type { Reviews } from "./reviews.ts";
+
 export type Tutor = {
   tutorId: number;
   name: string;
   profilePictureUrl: string;
+  tutorSummary: string;
   avgRating: number;
   reviewCount: number;
+  reviews: Reviews[];
   minGradeLevel: number;
   maxGradeLevel: number;
+  availableTimeSlots: TutorTimeslot[];
 };
+
+export type { TutorTimeslot };
