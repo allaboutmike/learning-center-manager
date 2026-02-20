c2d7c442598c00cb50a1ed64c4eab5bfae56c647
ermonio <moni.osborne@yahoo.com>
2026-02-20 12:39:32 -0600

    TUT- 19 Initialize Shadcn

--- frontend/react-ts-vite-app/vite.config.ts
+++ frontend/react-ts-vite-app/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +13,11 @@ export default defineConfig({
         target: "http://localhost:8080",
         changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
