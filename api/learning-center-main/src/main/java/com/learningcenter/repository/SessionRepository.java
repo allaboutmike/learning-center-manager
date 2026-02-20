1058c75e8ba16c3561d452a11642126a709af123
Yodit-Weldegeorgise <60828026+Yodit-Weldegeorgise@users.noreply.github.com>
2026-02-20 10:06:18 -0600

    Tut 41 parent service and dto implementation (#43)
    * Service and test added
    
    * Child response
    
    * Test keeps failing
    
    * ChildResponse added
    
    * Add test
    
    * Session service and Session Repo modification
    
    * Swagger dependency added
    
    * Change made after comment

--- api/learning-center-main/src/main/java/com/learningcenter/repository/SessionRepository.java
+++ api/learning-center-main/src/main/java/com/learningcenter/repository/SessionRepository.java
@@ -3,6 +3,7 @@ package com.learningcenter.repository;
 import com.learningcenter.entities.Session;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +11,7 @@ public interface SessionRepository extends CrudRepository<Session, Long> {
 
     @Query("SELECT s FROM Session s WHERE s.child.childId = :childId")
     List<Session> findSessionsByChildId(Long childId);
+
+    @Query("SELECT s FROM Session s WHERE s.child.childId = :childId AND s.child.parent.parentId = :parentId")
+    List<Session> findSessionsByParentIdAndChildId(@Param("parentId") Long parentId, @Param("childId") Long childId);
 }
