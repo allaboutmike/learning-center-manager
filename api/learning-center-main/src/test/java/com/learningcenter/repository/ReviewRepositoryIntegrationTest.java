e358aad516bbe0c0059db13e810e4040822b3b52
Nikhil Patel <89890904+nvpatel7@users.noreply.github.com>
2026-02-20 22:56:34 -0600

    Fixed review controller 404 errors(#58)
    - Ticket: [TUT-46](https://team-mike-tutoring.atlassian.net/browse/TUT-46)
    
    ### Proposed Changes
    Fixed 404 errors by setting correct fields in the response
    
    ### Testing

--- api/learning-center-main/src/test/java/com/learningcenter/repository/ReviewRepositoryIntegrationTest.java
+++ api/learning-center-main/src/test/java/com/learningcenter/repository/ReviewRepositoryIntegrationTest.java
@@ -1,12 +1,15 @@
 package com.learningcenter.repository;
 
-import com.learningcenter.entities.Review;
-import com.learningcenter.entities.Tutor;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
-import static org.assertj.core.api.Assertions.assertThat;
+
+import com.learningcenter.entities.Review;
+import com.learningcenter.entities.Tutor;
 
 @DataJpaTest
 public class ReviewRepositoryIntegrationTest {
@@ -20,9 +23,9 @@ public class ReviewRepositoryIntegrationTest {
     @Test
     void shouldFindTutorWithReviewCount() {
         Tutor tutor = new Tutor("Jane", 2, 4, "example.com", "testing 123");
-        Review review1 = new Review("testing 123", 4, tutor);
-        Review review2 = new Review("testing 123", 4, tutor);
-        Review review3 = new Review("testing 123", 4, tutor);
+        Review review1 = new Review("testing 123", 4, tutor, LocalDateTime.now());
+        Review review2 = new Review("testing 123", 4, tutor, LocalDateTime.now());
+        Review review3 = new Review("testing 123", 4, tutor, LocalDateTime.now());
         tutor.getReviews().add(review1);
         tutor.getReviews().add(review2);
         tutor.getReviews().add(review3);
@@ -38,7 +41,7 @@ public class ReviewRepositoryIntegrationTest {
     @Test
     void shouldFindTutorWithAverageRating() {
         Tutor tutor = new Tutor("John", 5, 7, "example.com", "testing 123");
-        Review review = new Review("testing 123", 4, tutor);
+        Review review = new Review("testing 123", 4, tutor, LocalDateTime.now());
         tutor.getReviews().add(review);
 
         entityManager.persist(tutor);
