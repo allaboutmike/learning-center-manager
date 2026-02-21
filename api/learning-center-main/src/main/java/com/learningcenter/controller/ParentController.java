e0599f232aef8d7fc4e61dce27b35d85bff50164
Brandon Hamilton <bham2015@gmail.com>
2026-02-20 22:53:04 -0600

    Created GET requests in the Parent Controller

--- api/learning-center-main/src/main/java/com/learningcenter/controller/ParentController.java
+++ api/learning-center-main/src/main/java/com/learningcenter/controller/ParentController.java
@@ -1,16 +1,50 @@
 package com.learningcenter.controller;
 
+import com.learningcenter.dto.ChildResponse;
+import com.learningcenter.dto.SessionResponse;
 import com.learningcenter.service.ParentService;
+import com.learningcenter.service.SessionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/parents")
 public class ParentController {
     private final ParentService parentService;
+    private final SessionService sessionService;
 
-    public ParentController(ParentService parentService) {
+    public ParentController(ParentService parentService, SessionService sessionService) {
         this.parentService = parentService;
+        this.sessionService = sessionService;
     }
 
+    //Handles GET request to get children associated to a parent by the parentId
+    @GetMapping("/{parentId}/children")
+    public List<ChildResponse> searchChildrenByParentId(@RequestParam(required = true) Long parentId) {
+        var children = parentService.getChildrenByParent(parentId);
+
+        return ResponseEntity.ok(children).getBody();
+    }
+
+    //Handles GET request to get a child's past sessions
+    @GetMapping("/{parentId}/children/{childId}/sessions/past")
+    public List<SessionResponse> getPastSessionsByChildId(@RequestParam(required = true) Long parentId, @RequestParam(required = true) Long childId) {
+        var pastSessions = sessionService.getPastSessions(parentId, childId);
+
+        return ResponseEntity.ok(pastSessions).getBody();
+    }
+
+    //Handles GET request to get a child's upcoming sessions
+    @GetMapping("/api/parents/{parentId}/children/{childId}/sessions/upcoming")
+    public List<SessionResponse> getUpcomingSessionsByParentIdAndChildId(@RequestParam(required = true) Long parentId, @RequestParam(required = true) Long childId) {
+        var upcomingSessions = sessionService.getUpcomingSessions(parentId, childId);
+
+        return ResponseEntity.ok(upcomingSessions).getBody();
+    }
 }
