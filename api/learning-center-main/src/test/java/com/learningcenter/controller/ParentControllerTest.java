2293dfa65d866b372ad79710e269178bcc8c25f6
CodenamePianist <110427623+CodenamePianist@users.noreply.github.com>
2026-02-21 23:30:50 -0600

    Tut 39 parent controller (#59)
    * Started on the Parent controller class
    
    * Created GET requests in the Parent Controller
    
    * Fixed endpoint to get list of children
    
    * Got upcoming and past session endpoints to work by changing RequestParam annotation to PathVariable
    
    * Created tests for the ParentController
    
    * Removed unnecessary var and white space

---
+++ api/learning-center-main/src/test/java/com/learningcenter/controller/ParentControllerTest.java
@@ -0,0 +1,56 @@
+package com.learningcenter.controller;
+
+import com.learningcenter.dto.ChildResponse;
+import com.learningcenter.dto.SessionResponse;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Transactional
+public class ParentControllerTest {
+
+    @Autowired
+    private ParentController parentController;
+
+    @Test
+    void searchMultipleChildrenByParentIdAndReturnChildList() {
+        List<ChildResponse> children = parentController.searchChildrenByParentId(2L);
+        assertNotNull(children);
+        assertEquals(3, children.size());
+    }
+
+    @Test
+    void searchSingleChildByParentIdAndReturnChildList() {
+        List<ChildResponse> children = parentController.searchChildrenByParentId(1L);
+        assertNotNull(children);
+        assertEquals(1, children.size());
+    }
+
+    @Test
+    void searchChildrenByParentIdAndReturnEmptyChildList() {
+        List<ChildResponse> children = parentController.searchChildrenByParentId(3L);
+        assertNotNull(children);
+        assertEquals(0, children.size());
+    }
+
+    @Test
+    void searchPastSessionsByChildIdAndParentIdAndReturnSessionsList() {
+        List<SessionResponse> pastSessions = parentController.getPastSessionsByParentIdAndChildId(1L, 1L);
+        assertNotNull(pastSessions);
+        assertEquals(0, pastSessions.size());
+    }
+
+    @Test
+    void searchUpcomingSessionsByChildIdAndParentIdAndReturnSessionsList() {
+        List<SessionResponse> upcomingSessions = parentController.getUpcomingSessionsByParentIdAndChildId(2L, 2L);
+        assertNotNull(upcomingSessions);
+        assertEquals(0, upcomingSessions.size());
+    }
+}
