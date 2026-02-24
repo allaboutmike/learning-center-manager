21f400db96f7e38421549e01055aaf7f458aae35
nvpatel7 <nvpatel2002@gmail.com>
2026-02-24 12:33:48 -0500

    Fixed prod by changing from rand to random

--- api/learning-center-main/src/main/resources/db/migration/V4__add_credits_to_parent.sql
+++ api/learning-center-main/src/main/resources/db/migration/V4__add_credits_to_parent.sql
@@ -2,4 +2,4 @@ alter table parent_account.parent
     add column credits INTEGER not null default 0;
 
 update parent_account.parent
-set credits = FLOOR(RAND()*(10-5+1)+5);
+set credits = FLOOR(random()*(10-5+1)+5);
