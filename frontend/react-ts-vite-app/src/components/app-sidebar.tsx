229c07d0749d867e242ff34ac583019b106bd8ba
Justin Alcendor <kareem4209@gmail.com>
2026-03-11 22:08:40 -0500

    TUT-125 Button Cleanup (#127)
    * TUT-125 Button Cleanup
    
    * bad build fix

--- frontend/react-ts-vite-app/src/components/app-sidebar.tsx
+++ frontend/react-ts-vite-app/src/components/app-sidebar.tsx
@@ -3,7 +3,6 @@ import {
   IconChartBar,
   IconDashboard,
   IconFolder,
-  IconInnerShadowTop,
   IconListDetails,
   IconUserPlus,
   IconShieldCheck,
@@ -154,9 +153,8 @@ export function AppSidebar({
               asChild
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <a href="/dashboard">
-                <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">
+              <a href="/">
+                <span className="p-4 text-base font-semibold text-green-500 min-w-8 duration-200 ease-linear">
                   Dallas Learning Central
                 </span>
               </a>
