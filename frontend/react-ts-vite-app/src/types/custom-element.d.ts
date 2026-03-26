e83715eab7a511dfb232a62d5d31b8399d8121cd
Yodit-Weldegeorgise <60828026+Yodit-Weldegeorgise@users.noreply.github.com>
2026-03-26 00:42:09 -0500

    Uncommited files from previous feature

--- frontend/react-ts-vite-app/src/types/custom-element.d.ts
+++ frontend/react-ts-vite-app/src/types/custom-element.d.ts
@@ -1,5 +1,16 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "white-board": any;
+import type React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "white-board": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        ref?: React.Ref<HTMLElement>;
+      };
+    }
   }
 }
+
+export {};
